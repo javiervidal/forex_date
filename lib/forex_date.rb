6e7e7b9bca65a17f06feca8b2a4716247c1a22d0
Javier Vidal <zanaguara@gmail.com>
2012-05-30 10:12:56 +0200

    version bump to 1.0.3, changelog added

--- lib/forex_date.rb
+++ lib/forex_date.rb
@@ -4,10 +4,10 @@ module ForexDate
 
   require 'tzinfo'
 
-  def ForexDate.now
+  def ForexDate.today
     t = Time.now.in_time_zone("Eastern Time (US & Canada)")
     date = t.to_date
-    date += 1.day if t.hour > 17
+    date += 1.day if t.hour >= 17
     return ( date.wday == 6 || date.wday == 7 ) ? nil : date
   end
 
